f9b99d91c32b1fee9a5091d77fb3a3322ccc7aa4
AlexLastoriaButler <122047127+AlexLastoriaButler@users.noreply.github.com>
2023-01-05 19:31:28 +0000

    Add files via upload

---
+++ readfiles.py
@@ -0,0 +1,36 @@
+import glob
+import os
+import time
+
+
+filelist = []
+
+for path, subdirs, files in os.walk(r'D:/'):
+        #filelist.append(os.path.join(path, name))
+    for file in files:
+        fcreated = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(os.path.getctime(os.path.join(path, file)))) 
+        fmodified = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(os.path.getmtime(os.path.join(path, file)))) 
+        fsize = str(round(os.path.getsize(os.path.join(path, file))/1024,0))
+        filelist.append(fcreated + ' - ' + fmodified + ' - ' + fsize + ' - ' + os.path.join(path, file))
+
+
+
+
+import pandas as pd
+
+
+
+test = [sub.replace(' - ', ' #-# ', 3) for sub in filelist]
+test_df = pd.DataFrame(test, columns=['col1'])
+test_df[['created','modified','size','name']] = test_df['col1'].str.split(' #-# ',expand=True)
+
+
+test_df.to_csv('D:/filelist_20221126_2.csv', index=False)
+
+
+
+my_df = pd.DataFrame(filelist, columns=['col1'])
+my_df.to_csv('D:/filelist_20221126.csv', index=False)
+
+
+test_df['size'] = test_df['size'].astype(float)
